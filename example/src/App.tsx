2edecbe1b7fa2130eccd941deac3d43a16e97a13
Breno Christovam Ramos <breno.christovam@iesb.edu.br>
2023-10-12 18:00:50 -0300

    feat: add login screen to login-module

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,27 @@
-import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'login-module';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  // const [result, setResult] = React.useState<number | undefined>();
 
-  React.useEffect(() => {
-    multiply(300, 70).then(setResult);
-  }, []);
+  // React.useEffect(() => {
+  //   multiply(300, 70).then(setResult);
+  // }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
-}
+//   return (
+//     <View style={styles.container}>
+//       <Text>Result: {result}</Text>
+//     </View>
+//   );
+ }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   box: {
+//     width: 60,
+//     height: 60,
+//     marginVertical: 20,
+//   },
+// });
