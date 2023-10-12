2b59e4109495362582eed91a10192836b8b5a8e3
Breno Christovam Ramos <breno.christovam@iesb.edu.br>
2023-10-12 04:37:45 -0300

    feat: first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,28 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
+import {api} from 'http-module';
+
+export interface IUserResponse {
+  name: string;
+  email: string;
+};
+
+export interface ILoginUser {
+  email: string;
+  password: string;
+};
+
+export interface ISignUpUser {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+};
+
+export const fetchUsers = async(): Promise<IUserResponse> => {
+  const response = await api.get<IUserResponse>('/read-user');
+  return response.data;
+};
+
+export const login = async(user: ILoginUser): Promise<Number> => {
+  const response = await api.post('/login-user', user);
+  return response.status;
+};
